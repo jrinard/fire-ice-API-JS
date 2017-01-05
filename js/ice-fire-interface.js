a724f86b90748aea5b084a008ad5dd4c21c36936
Erik Killops and Joshua Rinard <joshua@>
2017-01-04 16:38:14 -0800

    adjust spacing

--- js/ice-fire-interface.js
+++ js/ice-fire-interface.js
@@ -3,19 +3,17 @@ var House = require('./../js/ice-fire.js').houseModule;
 var displayHouseInfo = function(houseObject) {
   var houseInfo = houseObject.info;
   var houseHtml =
-    '<img class="bigimage" src="./img/' + houseObject.nameTag + '.jpg"/>'+
-    '<h2>' + houseInfo.name + '</h2>' +
-    '<h4><span class="titles">Region: </span>' + houseInfo.region + '</h4>' +
-    '<h4><span class="titles">Coat of Arms: </span>' + houseInfo.coatOfArms + '</h4>' +
-    '<h4><span class="titles">Slogan: </span>' + houseInfo.words + '</h4>' +
-    '<h4><span class="titles">Titles: </span>' + houseInfo.titles[0] + ', ' +    houseInfo.titles[1] + ', ' + houseInfo.titles[2] + '</h4>' +
-    '<h4><span class="titles">Ancestral Weapons: </span>' + houseInfo.ancestralWeapons[0] + '</h4>';
-  // '<h2>' + houseInfo.name + '</h2>' +
-  //                 '<h4><span class="titles">Region: </span>' + houseInfo.region + '</h4>' +
-  //                 '<h4><span class="titles">Coat of Arms: </span>' + houseInfo.coatOfArms + '</h4>' +
-  //                 '<h4><span class="titles">Slogan: </span>' + houseInfo.words + '</h4>' +
-  //                 '<h4><span class="titles">Titles: </span>' + houseInfo.titles[0] + ', ' +    houseInfo.titles[1] + ', ' + houseInfo.titles[2] + '</h4>' +
-  //                 '<h4><span class="titles">Ancestral Weapons: </span>' + houseInfo.ancestralWeapons[0] + '</h4>';
+  '<div class="row">' + '<div class="col-sm-4">' +
+  '<img class="bigimage" src="./img/' + houseObject.nameTag + '.jpg"/>'+'</div>'+
+  '<div class="col-sm-8">' +
+  '<h2>' + houseInfo.name + '</h2>' +
+  '<h4><span class="titles">Region: </span>' + houseInfo.region + '</h4>' +
+  '<h4><span class="titles">Coat of Arms: </span>' + houseInfo.coatOfArms + '</h4>' +
+  '<h4><span class="titles">Slogan: </span>' + houseInfo.words + '</h4>' +
+  '<h4><span class="titles">Titles: </span>' + houseInfo.titles[0] + ', ' +    houseInfo.titles[1] + ', ' + houseInfo.titles[2] + '</h4>' +
+  '<h4><span class="titles">Ancestral Weapons: </span>' + houseInfo.ancestralWeapons[0] + '</h4>'
+  + '</div>' + '</div>';
+
   $("#showResult").html(houseHtml);
 };
 
