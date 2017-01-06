1970a2a3c99b67378655e5dce4a0aa903b9194d4
Erik Killops and Joshua Rinard <erik+joshua@>
2017-01-05 16:23:03 -0800

    searching works

--- js/ice-fire-interface.js
+++ js/ice-fire-interface.js
@@ -1,22 +1,89 @@
 var House = require('./../js/ice-fire.js').houseModule;
+var Character = require('./../js/ice-fire.js').characterModule;
+var Book = require('./../js/ice-fire.js').bookModule;
 var Searcher = require('./../js/ice-fire.js').searcherModule;
 
 var displayHouseInfo = function(houseObject) {
   var houseInfo = houseObject.info;
+  var validImages = ["stark", "arryn", "baratheon", "bracken", "clegane", "greyjoy", "lannister", "mallister", "martell", "mormont", "targaryen", "tully", "tyrell"];
+  var houseImage;
+  if(validImages.includes(houseObject.nameTag)) {
+    houseImage = "./img/" + houseObject.nameTag + ".jpg";
+  } else {
+    houseImage = "./img/house.png";
+  }
+  // var houseImage = "./img/house.png";
   var houseHtml =
   '<div class="row">' + '<div class="col-sm-4">' +
-  '<img class="bigimage" src="./img/' + houseObject.nameTag + '.jpg"/>'+'</div>'+
+  '<img class="bigimage" src="'+houseImage+'"/>'+'</div>'+
   '<div class="col-sm-8">' +
   '<h2>' + houseInfo.name + '</h2>' +
   '<h4><span class="titles">Region: </span>' + houseInfo.region + '</h4>' +
   '<h4><span class="titles">Coat of Arms: </span>' + houseInfo.coatOfArms + '</h4>' +
   '<h4><span class="titles">Slogan: </span>' + houseInfo.words + '</h4>' +
-  '<h4><span class="titles">Titles: </span>' + houseInfo.titles[0] + ', ' +    houseInfo.titles[1] + ', ' + houseInfo.titles[2] + '</h4>' +
-  '<h4><span class="titles">Ancestral Weapons: </span>' + houseInfo.ancestralWeapons[0] + '</h4>' +
+  '<h4><span class="titles">Titles: </span>' + houseInfo.titles + '</h4>' +
+  '<h4><span class="titles">Ancestral Weapons: </span>' + houseInfo.ancestralWeapons + '</h4>' +
   '</div>' + '</div>';
 
   $("#showResult").html(houseHtml);
 };
+
+var displayCharacterInfo = function(characterObject) {
+  var characterInfo = characterObject.info;
+  var characterHtml =
+  '<div class="row">' + '<div class="col-sm-4">' +
+  '<img class="bigimage" src="./img/char.png"/>'+'</div>'+
+  '<div class="col-sm-8">' +
+  '<h2>' + characterInfo.name + '</h2>' +
+  '<h4><span class="titles">Gender: </span>' + characterInfo.gender + '</h4>' +
+  '<h4><span class="titles">Culture: </span>' + characterInfo.culture + '</h4>' +
+  '<h4><span class="titles">Born: </span>' + characterInfo.born + '</h4>' +
+  '<h4><span class="titles">Titles: </span>' + characterInfo.titles + '</h4>' +
+  '<h4><span class="titles">Aliases: </span>' + characterInfo.aliases + '</h4>' +
+  '</div>' + '</div>';
+  $("#showResult").html(characterHtml);
+};
+
+var displaySearchInfo = function(searchObject) {
+  var searchHtml = '<ul><h2>Search Results:<span id="cat" value="'+searchObject.category+'"></span></h2></ul>';
+  $("#showResult").html(searchHtml);
+  var searchInfo = searchObject.infoList;
+  searchInfo.forEach(function(element){
+    var searchHtml;
+    if(searchObject.category === "characters") {
+      searchHtml =
+      '<br>'+'<li>'+
+        '<h4 value="'+element.Name+'"><span class="titles">Name:</span> ' + element.Name+' &nbsp;&nbsp;&nbsp; <span value="'+element.Id+'" class="btn btn-xs btn-default viewInfo">View Info</span></h4>' +
+        '<h4><span class="titles">Aliases:</span> ' + element.Aliases+'</h4>' +
+      '</li>'+'<hr>';
+    } else {
+      searchHtml =
+      '<br>'+'<li value="'+element.Id+'">'+
+      '<h4 value="'+element.Name+'"><span class="titles">Name:</span> ' + element.Name+' &nbsp;&nbsp;&nbsp; <span value="'+element.Id+'" class="btn btn-xs btn-default viewInfo">View Info</span></h4>' +
+      '</li>'+'<hr>';
+    }
+    $('#showResult ul').append(searchHtml);
+    $(".viewInfo").on("click", viewInfo);
+  });
+};
+
+var viewInfo = function(){
+  var viewId = $(this).attr('value');
+  var viewName = $(this).parent().attr('value');
+  var viewCategory = $('#cat').attr('value');
+  $('#showResult').empty().show();
+  if (viewCategory === 'characters') {
+    var newCharacter = new Character(viewId, viewName);
+    newCharacter.getInfo(displayCharacterInfo);
+  } else if (viewCategory === 'books') {
+    var newBook = new Book(viewId, viewName);
+    newBook.getInfo(displayBookInfo);
+  } else if (viewCategory === 'houses') {
+    var newHouse = new House(viewId, viewName);
+    newHouse.getInfo(displayHouseInfo);
+  }
+};
+
 
 $(document).ready(function() {
   $('img').click(function() {
@@ -33,11 +100,13 @@ $(document).ready(function() {
   });
 
   $('.search-link').click(function() {
+    $('#house-box').hide();
     var searchCategory = $(this).attr('id');
     var searchText = $('#search-text').val();
     var newSearcher = new Searcher(searchCategory,searchText);
-    var finalList = newSearcher.search();
-    console.log(newSearcher.infoList);
+    newSearcher.search();
+    $('#search-text').val("");
+    $('#showResult').empty().show();
+    displaySearchInfo(newSearcher);
   });
-
 });
--- js/ice-fire.js
+++ js/ice-fire.js
@@ -2,6 +2,40 @@ var books = require('../json_data/booksClean.json');
 var houses = require('../json_data/housesClean.json');
 var characters = require('../json_data/charactersClean.json');
 
+function Book(newId, newName) {
+  this.id = newId;
+  this.nameTag = newName;
+  this.info = null;
+}
+
+Book.prototype.getInfo = function(displayFunction) {
+  var that = this;
+  $.get('http://www.anapioficeandfire.com/api/books/' + that.id).then(
+    function(response){
+      that.info = response;
+      displayFunction(that);
+    }).fail(function(error) {
+      console.log(error.responseJSON.message);
+    });
+  };
+
+function Character(newId, newName) {
+  this.id = newId;
+  this.nameTag = newName;
+  this.info = null;
+}
+
+Character.prototype.getInfo = function(displayFunction) {
+  var that = this;
+  $.get('http://www.anapioficeandfire.com/api/characters/' + that.id).then(
+    function(response){
+      that.info = response;
+      displayFunction(that);
+    }).fail(function(error) {
+      console.log(error.responseJSON.message);
+    });
+  };
+
 function House(newId, newName) {
   this.id = newId;
   this.nameTag = newName;
@@ -13,11 +47,13 @@ House.prototype.getInfo = function(displayFunction) {
   $.get('http://www.anapioficeandfire.com/api/houses/' + that.id).then(
     function(response){
       that.info = response;
+      var houseNameWords = that.info.name.split(" ");
+      that.nameTag = houseNameWords[1].toLowerCase();
       displayFunction(that);
+    }).fail(function(error) {
+      console.log(error.responseJSON.message);
     });
   };
-
-exports.houseModule = House;
 
 
 function Searcher(category, searchValue) {
@@ -30,19 +66,32 @@ Searcher.prototype.search = function() {
   var that = this;
   if (that.category === 'books') {
     for (var i=0; i < books.length; i++) {
-      var book = books[i];
-      for (var key in book) {
-        var keyValue = book[key].toString().toLowerCase();
-        if (keyValue.includes(that.searchValue)) {
-          that.infoList.push(book);
-        }
+      if (books[i].Name.toLowerCase().includes(that.searchValue)) {
+        that.infoList.push(books[i]);
       }
     }
   } else if (this.category === 'houses') {
-    // do stuff
+    for (var j=0; j < houses.length; j++) {
+      if (houses[j].Name.toLowerCase().includes(that.searchValue)) {
+        that.infoList.push(houses[j]);
+      }
+    }
   } else if (this.category === 'characters') {
-    // do stuff
+    for (var k=0; k < characters.length; k++) {
+      if (characters[k].Name.toLowerCase().includes(that.searchValue)) {
+        that.infoList.push(characters[k]);
+      } else {
+        for(var alias in characters[k].Aliases) {
+          if (alias.toLowerCase().includes(that.searchValue)) {
+            that.infoList.push(characters[k]);
+          }
+        }
+      }
+    }
   }
 };
 
+exports.houseModule = House;
+exports.characterModule = Character;
+exports.bookModule = Book;
 exports.searcherModule = Searcher;
